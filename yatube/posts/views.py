d8d777364a32180839667e2be03624987f2bd389
Марат Мелкумян <melkumyanmarat9@gmail.com>
2022-06-21 01:03:34 +0300

    project

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,21 +4,21 @@ from .models import Post, Group
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
-    title =  'Это главная страница проекта Yatube'
+    title = 'Это главная страница проекта Yatube'
     context = {
         'posts': posts,
         'title': title
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    title =  f'Записи сообщества {group}'
+    title = f'Записи сообщества {group}'
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
         'title': title
     }
-    return render(request, 'posts/group_list.html', context) 
+    return render(request, 'posts/group_list.html', context)
