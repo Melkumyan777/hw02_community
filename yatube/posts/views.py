bc9c909ae54ad57d2cf7464d5d135dad3a7d00e3
Марат Мелкумян <melkumyanmarat9@gmail.com>
2022-06-23 00:08:10 +0300

    project

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -15,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()
+    posts = group.posts.all()[:10]
     context = {
         "group": group,
         "posts": posts
